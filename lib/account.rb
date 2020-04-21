c92a49fb97fd894dd2ef5b7224eb7d97cdddb592
plkujaw <jawor.jakub@gmail.com>
2020-04-21 09:17:37 +0100

    renamed files

---
+++ lib/account.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative 'transaction'
+require 'date'
+
+class Account
+  attr_reader :balance, :history
+
+  def initialize
+    @balance = 0
+    @history = []
+  end
+
+  def deposit(amount)
+    @balance += amount
+    deposit = Transaction.new('deposit', amount, @balance)
+    @history << deposit
+  end
+
+  def withdraw(amount)
+    @balance -= amount
+    withdrawal = Transaction.new('withdrawal', amount, @balance)
+    @history << withdrawal
+  end
+
+  def print_statement; end
+end
---
+++ spec/account_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'account'
+
+describe Account do
+  subject(:account) { described_class.new }
+  it 'has an opening balance of 0' do
+    expect(account.balance).to eq(0)
+  end
+  it 'can accept deposits' do
+    expect { account.deposit(1000) }.not_to raise_error
+  end
+  it 'can accept withdrawals' do
+    expect { account.withdraw(1000) }.not_to raise_error
+  end
+  it 'can print a statement' do
+    expect { account.print_statement }.not_to raise_error
+  end
+
+  describe '#deposit' do
+    it 'adds amount to the account balance' do
+      account.deposit(1000)
+      expect(account.balance).to eq(1000)
+    end
+    it 'stores an operation in transaction history' do
+      deposit = account.deposit(1000)
+      expect(account.history).to include(*deposit)
+    end
+  end
+
+  describe '#withdrawal' do
+    it 'deducts amount from the account balance' do
+      account.deposit(1000)
+      account.withdraw(500)
+      expect(account.balance).to eq(500)
+    end
+    it 'stores an operation in transaction history' do
+      withdraw = account.withdraw(1000)
+      expect(account.history).to include(*withdraw)
+    end
+  end
+end
